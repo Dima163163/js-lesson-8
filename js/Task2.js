eb0e2bc3bbcee85001be35469f234e3d209ba5ab
Dima163163 <khlopovdima@gmail.com>
2023-12-05 15:11:46 +0400

    functions and prototype

--- js/Task2.js
+++ js/Task2.js
@@ -1,55 +1,55 @@
-'use strict';
+// 'use strict';
 
-const countIp = (arr) => {
-  let count = 0;
+// const countIp = (arr) => {
+//   let count = 0;
 
-  const filterArr = arr.filter((item, index) => arr.indexOf(item) === index);
-  for (const key of filterArr) {
-    count++;
-  }
+//   const filterArr = arr.filter((item, index) => arr.indexOf(item) === index);
+//   for (const key of filterArr) {
+//     count++;
+//   }
 
-  return count;
-};
+//   return count;
+// };
 
-const countIpSet = (arr) => {
-  let count = 0;
-  const newArr = new Set();
+// const countIpSet = (arr) => {
+//   let count = 0;
+//   const newArr = new Set();
 
-  for (const key of arr) {
-    newArr.add(key);
-  }
+//   for (const key of arr) {
+//     newArr.add(key);
+//   }
 
-  for (const key of newArr) {
-    count++;
-  }
+//   for (const key of newArr) {
+//     count++;
+//   }
 
-  return count;
-};
+//   return count;
+// };
 
-const listIPv4 = [
-  "10.90.112.24",
-  "10.53.161.190",
+// const listIPv4 = [
+//   "10.90.112.24",
+//   "10.53.161.190",
 
-  "10.205.116.71",
+//   "10.205.116.71",
 
-  "10.71.83.95",
-  "10.53.161.190",
-  "10.106.136.132",
-  "10.53.161.190",
+//   "10.71.83.95",
+//   "10.53.161.190",
+//   "10.106.136.132",
+//   "10.53.161.190",
 
-  "10.248.129.60",
-  "10.83.161.244",
-  "10.54.99.35",
-  "10.249.150.138",
+//   "10.248.129.60",
+//   "10.83.161.244",
+//   "10.54.99.35",
+//   "10.249.150.138",
 
-  "10.246.92.188",
-  "10.106.80.33",
-  "10.171.137.197",
-  "10.205.191.187",
-  "10.201.184.28",
-  "10.213.145.216",
-  "10.74.3.33"
-];
+//   "10.246.92.188",
+//   "10.106.80.33",
+//   "10.171.137.197",
+//   "10.205.191.187",
+//   "10.201.184.28",
+//   "10.213.145.216",
+//   "10.74.3.33"
+// ];
 
-console.log(`Количество уникальных Id: ${countIp(listIPv4)}`);
-console.log(`Количество уникальных Id (Set): ${countIpSet(listIPv4)}`);
+// console.log(`Количество уникальных Id: ${countIp(listIPv4)}`);
+// console.log(`Количество уникальных Id (Set): ${countIpSet(listIPv4)}`);
--- js/cart.js
+++ js/cart.js
@@ -1,54 +1,114 @@
 'use strict';
 
-const cart = {
-  items: [],
-  count: 0,
-  get totalPrice() {
-    return this.calculateItemPrice();
-  },
-  add(name, price, count = 1) {
-    const obj = {
-      name,
-      price,
-      count,
-    };
+// const cart = {
+//   items: [],
+//   count: 0,
+//   get totalPrice() {
+//     return this.calculateItemPrice();
+//   },
+//   add(name, price, count = 1) {
+//     const obj = {
+//       name,
+//       price,
+//       count,
+//     };
+//     this.increaseCount();
+//     this.items.push(obj);
+//     this.calculateItemCount();
+//   },
+//   increaseCount(numb = 0) {
+//     this.count += numb;
+//   },
+//   calculateItemPrice() {
+//     return this.items.reduce((acc, item) =>
+//       acc += (item.price * item.count), 0);
+//   },
+//   calculateItemCount() {
+//     this.count = this.items.reduce((acc, item) =>
+//       acc += item.count
+//     , 0);
+//   },
+//   clear() {
+//     this.items = [];
+//   },
+//   print() {
+//     console.log(`Товары: ${JSON.stringify(this.items)} 
+//       \n 
+//       Общая стоимость: ${this.totalPrice}
+//       \n
+//       Общее количество товаров: ${this.count}
+//     `);
+//   },
+// };
+
+// cart.add('Принтер', 3600, 4);
+// cart.add('iMac', 160000, 1);
+// cart.add('iPhone', 170000, 2);
+// cart.add('iPad', 90000, 4);
+// cart.add('Телевизор', 105000, 5);
+// cart.add('Чайник', 5000, 6);
+
+
+// cart.print();
+
+const Cart = function(arr = []) {
+  this.goods = arr;
+  this.totalPrice = 0;
+  this.count = 0;
+  this.addGoods = function(obj) {
+    arr.push(obj);
     this.increaseCount();
-    this.items.push(obj);
-    this.calculateItemCount();
-  },
-  increaseCount(numb = 0) {
-    this.count += numb;
-  },
-  calculateItemPrice() {
-    return this.items.reduce((acc, item) =>
-      acc += (item.price * item.count), 0);
-  },
-  calculateItemCount() {
-    this.count = this.items.reduce((acc, item) =>
-      acc += item.count
-    , 0);
-  },
-  clear() {
-    this.items = [];
-  },
-  print() {
-    console.log(`Товары: ${JSON.stringify(this.items)} 
-      \n 
-      Общая стоимость: ${this.totalPrice}
-      \n
-      Общее количество товаров: ${this.count}
-    `);
-  },
+  };
+  this.calculateGoodsPrice = function() {
+    this.totalPrice = this.goods.reduce((acc, good) =>
+      acc += good.discount ?
+      good.price - (good.price * good.discount / 100) : good.price, 0);
+  };
+  this.getTotalPrice = function() {
+    return this.totalPrice;
+  };
+  this.increaseCount = function() {
+    this.count += 1;
+  };
+  this.clear = function() {
+    this.goods = [];
+    this.totalPrice = 0;
+    this.count = 0;
+  };
+  this.print = function() {
+    console.log(`
+    Товары: ${JSON.stringify(this.goods)}
+    \n 
+    Общая стоимость: ${this.totalPrice}`);
+  };
+};
+const Goods = function(name, price, discount) {
+  this.name = name;
+  this.price = price;
+  this.discount = discount;
 };
 
+const FoodGoods = function(name, price, discount, calories) {
+  Goods.call(this, name, price, discount);
+  this.calories = calories;
+};
+const ClothingGoods = function(name, price, discount, material) {
+  Goods.call(this, name, price, discount);
+  this.material = material;
+};
+const TechnicsGoods = function(name, price, discount, typeTechnic) {
+  Goods.call(this, name, price, discount);
+  this.typeTechnic = typeTechnic;
+};
 
+const cards = new Cart();
+const imac = new TechnicsGoods('iMac', 16000, 5, 'моноблоки');
+const iphone = new ClothingGoods('iPhone', 140000, 0, 'aluminum');
+const bigMac = new FoodGoods('Big Mac', 300, 0, 400);
 
-cart.add('Принтер', 3600, 4);
-cart.add('iMac', 160000, 1);
-cart.add('iPhone', 170000, 2);
-cart.add('iPad', 90000, 4);
-cart.add('Телевизор', 105000, 5);
-cart.add('Чайник', 5000, 6);
-
-
-cart.print();
+cards.addGoods(imac);
+cards.addGoods(iphone);
+cards.addGoods(bigMac);
+cards.calculateGoodsPrice();
+cards.getTotalPrice();
+console.log(cards.print());
