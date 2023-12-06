5b9fdecfaf8c426ac5b71c8328519cb7c9cd45c9
Dima163163 <khlopovdima@gmail.com>
2023-12-06 13:15:00 +0400

    functions and prototype

--- js/cart.js
+++ js/cart.js
@@ -1,36 +1,34 @@
 'use strict';
 
 const Cart = function(arr = []) {
-  return {
-    goods: arr,
-    totalPrice: 0,
-    count: 0,
-    addGoods(obj) {
-      arr.push(obj);
-      this.increaseCount();
-    },
-    calculateGoodsPrice() {
-      this.totalPrice = this.goods.reduce((acc, good) =>
-        acc += good.discount ?
-        good.price - (good.price * good.discount / 100) : good.price, 0);
-    },
-    getTotalPrice() {
-      return this.totalPrice;
-    },
-    increaseCount() {
-      this.count += 1;
-    },
-    clear() {
-      this.goods = [];
-      this.totalPrice = 0;
-      this.count = 0;
-    },
-    print() {
-      console.log(`
-      Товары: ${JSON.stringify(this.goods)}
-      \n 
-      Общая стоимость: ${this.totalPrice}`);
-    },
+  this.goods = arr;
+  this.totalPrice = 0;
+  this.count = 0;
+  this.addGoods = function(obj) {
+    arr.push(obj);
+    this.increaseCount();
+  };
+  this.calculateGoodsPrice = function() {
+    this.totalPrice = this.goods.reduce((acc, good) =>
+      acc += good.discount ?
+      good.price - (good.price * good.discount / 100) : good.price, 0);
+  };
+  this.getTotalPrice = function() {
+    return this.totalPrice;
+  };
+  this.increaseCount = function () {
+    this.count += 1;
+  };
+  this.clear = function() {
+    this.goods = [];
+    this.totalPrice = 0;
+    this.count = 0;
+  };
+  this.print = function() {
+    console.log(`
+    Товары: ${JSON.stringify(this.goods)}
+    \n 
+    Общая стоимость: ${this.totalPrice}`);
   };
 };
 
