a27da573f7a1b879783c35688d03f050313a5aed
Dima163163 <khlopovdima@gmail.com>
2023-12-06 07:53:51 +0400

    functions and prototype

--- js/cart.js
+++ js/cart.js
@@ -1,56 +1,5 @@
 'use strict';
 
-// const cart = {
-//   items: [],
-//   count: 0,
-//   get totalPrice() {
-//     return this.calculateItemPrice();
-//   },
-//   add(name, price, count = 1) {
-//     const obj = {
-//       name,
-//       price,
-//       count,
-//     };
-//     this.increaseCount();
-//     this.items.push(obj);
-//     this.calculateItemCount();
-//   },
-//   increaseCount(numb = 0) {
-//     this.count += numb;
-//   },
-//   calculateItemPrice() {
-//     return this.items.reduce((acc, item) =>
-//       acc += (item.price * item.count), 0);
-//   },
-//   calculateItemCount() {
-//     this.count = this.items.reduce((acc, item) =>
-//       acc += item.count
-//     , 0);
-//   },
-//   clear() {
-//     this.items = [];
-//   },
-//   print() {
-//     console.log(`Товары: ${JSON.stringify(this.items)} 
-//       \n 
-//       Общая стоимость: ${this.totalPrice}
-//       \n
-//       Общее количество товаров: ${this.count}
-//     `);
-//   },
-// };
-
-// cart.add('Принтер', 3600, 4);
-// cart.add('iMac', 160000, 1);
-// cart.add('iPhone', 170000, 2);
-// cart.add('iPad', 90000, 4);
-// cart.add('Телевизор', 105000, 5);
-// cart.add('Чайник', 5000, 6);
-
-
-// cart.print();
-
 const Cart = function(arr = []) {
   this.goods = arr;
   this.totalPrice = 0;
@@ -67,7 +16,7 @@ const Cart = function(arr = []) {
   this.getTotalPrice = function() {
     return this.totalPrice;
   };
-  this.increaseCount = function() {
+  this.increaseCount = function () {
     this.count += 1;
   };
   this.clear = function() {
@@ -82,23 +31,34 @@ const Cart = function(arr = []) {
     Общая стоимость: ${this.totalPrice}`);
   };
 };
-const Goods = function(name, price, discount) {
-  this.name = name;
-  this.price = price;
-  this.discount = discount;
+
+class Goods {
+  constructor(name, price, discount) {
+    this.name = name;
+    this.price = price;
+    this.discount = discount;
+  };
 };
 
-const FoodGoods = function(name, price, discount, calories) {
-  Goods.call(this, name, price, discount);
-  this.calories = calories;
+class FoodGoods extends Goods {
+  constructor(name, price, discount, calories) {
+    super(name, price, discount);
+    this.calories = calories;
+  }
 };
-const ClothingGoods = function(name, price, discount, material) {
-  Goods.call(this, name, price, discount);
-  this.material = material;
+
+class ClothingGoods extends Goods{
+  constructor(name, price, discount, material) {
+    super(name, price, discount);
+    this.material = material;
+  }
 };
-const TechnicsGoods = function(name, price, discount, typeTechnic) {
-  Goods.call(this, name, price, discount);
-  this.typeTechnic = typeTechnic;
+
+class  TechnicsGoods extends Goods{
+  constructor(name, price, discount, typeTechnic) {
+    super(name, price, discount);
+    this.typeTechnic = typeTechnic;
+  }
 };
 
 const cards = new Cart();
